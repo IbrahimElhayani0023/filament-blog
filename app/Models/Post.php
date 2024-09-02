249f2549942ac96149ba98a11545a7d571d585ea
Ibrahim Elhayani <ibrahimelh23@gmail.com>
2024-09-02 22:24:40 +0100

    create laravel project with filament and build post and category crud

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'image',
+        'content',
+        'is_active',
+        'published_at',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+}
